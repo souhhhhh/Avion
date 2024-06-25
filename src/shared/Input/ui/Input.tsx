5c58994b73b2bcd887c5121a052da075f9e3415c
souhhh <153848345+souhhhhh@users.noreply.github.com>
2024-06-26 01:56:02 +0300

    input

--- src/shared/Input/ui/Input.tsx
+++ src/shared/Input/ui/Input.tsx
@@ -1,54 +1,78 @@
-import { useState } from 'react'
+import { useEffect } from 'react';
 import { Button } from '../../Button'
 import { ButtonTheme } from '../../Button/ui/Button'
 import styles from './Input.module.scss'
-import { SubmitErrorHandler, SubmitHandler, useForm } from 'react-hook-form'
+import {  useForm } from 'react-hook-form';
 interface Input { 
     email: string;
 }
+
+
 export const Input = () => { 
-    const {register, handleSubmit, formState: { errors } } = useForm<Input>({
+    const {register, handleSubmit, reset, formState: { errors, isSubmitSuccessful } } = useForm<Input>({
     })
+    
 
-    const submit: SubmitHandler<Input> = data => { 
-        console.log(data)
+    // const ClickClearInput = () => { 
+    //     setInput('')
+    // }
+    const onSubmit = () => { 
+        reset()
+        
     }
-
-    const error: SubmitErrorHandler<Input> = data => { 
-        console.log(data)
-    }
-
-    const [input, setInput] = useState('')
-
-    const ChangeInput = (event) => { 
-        setInput(event.target.value)
-    }
-
-    const ClickClearInput = () => { 
-        setInput('')
+    const Time = () => { 
+        return <span>Приглашение отправлено</span>
+        reset()
     }
     return ( 
         <div className={styles.InputBUtton}>
-            <form onSubmit={handleSubmit(submit, error)}>
-            <input {...register('email', {
-                required: true, 
-                minLength: {
-                    value: 5,
-                    message: 'Не меньше 10 символов'
-                },
-                pattern: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
-                maxLength: {
-                    value: 126,
-                    message: 'Не более 126 символов'
-                    }
-                    })} value={input} onChange={ChangeInput} className={styles.input} type="email" placeholder="your@email.com"/>
-                    <div>
-                        {errors?.email && <p>{errors?.email?.message || `${error.name}`}</p>}
-                    </div>
-            </form>
+
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+                <label className={styles.label} htmlFor="">
+                    <input {...register('email', { 
+                        required: true, 
+                        minLength: { 
+                            value: 5,
+                            message: 'Почта должна содержать не менее 5 символов'
+                        },
+                        pattern: {
+                            value: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                            message: `Введите корректную почту,  
+                            например: yourName@mail.ru`
+                            },
+                        maxLength: { 
+                            value: 126,
+                            message: 'Почта должна содержать не более 126 символов'
+                        }
+                    })} type='email' className={styles.input} placeholder='your@email.com'/>
+
+
                 <div className={styles.button}>
-                <span onClick={ClickClearInput}><Button theme={ButtonTheme.PURPLE}>Sign up</Button></span>
+                <span onSubmit={handleSubmit(onSubmit)} ><Button theme={ButtonTheme.PURPLE}>Sign up</Button></span>
+
+                    
                 </div>
+                </label>
+                    <div className={styles.success}>
+                       { isSubmitSuccessful === true ? <span>Приглашение отправлено</span> : null}
+                    </div>
+                <div className={errors.email && styles.error}>
+                    {errors?.email && <span>{errors?.email?.message}</span>}
+                </div>
+                
+                <div>
+
+                </div>
+                
+
+
+
+            </form>
+
+
+
         </div>
     )
 }
